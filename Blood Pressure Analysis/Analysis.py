b56d32c531961b189f6700f184f643fdf2f7cde5
lozanoFacundo <93389710+lozanoFacundo@users.noreply.github.com>
2021-11-05 15:41:08 -0300

    Update Analysis.py

--- Blood Pressure Analysis/Analysis.py
+++ Blood Pressure Analysis/Analysis.py
@@ -4,3 +4,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+df=pd.read_csv("data.csv", sep=';')
+df
+
+df.info() 
+
+"""
+It can be seen that the height and PA_max values are objects, this is because they are separated by a comma and not by a period. 
+Therefore, as a first measure, the comma should be changed, for this the data is loaded again considering that the decimal separator is the comma
+
+"""
+
